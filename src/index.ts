a2e8a71775d1f7d296c288060857ad95414affcb
Abhishek Prakash2 <abhishek.prakash2@bajajfinserv.in>
2022-01-06 03:30:43 +0530

    Setup cli structure

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,25 @@
-export * from './lib/async';
-export * from './lib/number';
+#! /usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+const DEFAULT_WORKING_DIRECTORY = process.cwd();
+
+program
+    .version("1.0.0")
+    .description("The image manipulation library.")
+    .option("-r, --recursive <bool>", "Recursively process the files in subdirectories", true);
+
+program
+    .command("convert <format> <source> [destination]")
+    .description("convert the file into the desired format")
+    .action(function (format, source, destination = DEFAULT_WORKING_DIRECTORY) {
+        console.log(
+            format,
+            source,
+            destination,
+            program.opts()
+        );
+    });
+
+program.parse(process.argv);
